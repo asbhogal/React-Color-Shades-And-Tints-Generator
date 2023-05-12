fbc18a53f7cef529216f0ee974e994cb28e89c2e
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-05-12 14:44:39 +0100

    added pseudocode for potential logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+// take in hex code
+// check validity of input (ie. valid hex code)
+// convert hex to rgb value
+// create two arrays of 10 values, each with 10% increments, for lightness and darkness
+// map through and return these front-end
+// display original hex code (no logic required)
+
+
 function App() {
   return <></>;
 }
